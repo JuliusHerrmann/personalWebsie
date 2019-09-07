9f8ff956d9a2f3c8c4e68f79745e6d4e09f5f8a7
Julius Herrmann <herrmann.julius@gmx.de>
2019-09-07 20:04:16 +0200

    animated plane

--- res/animatePlane.js
+++ res/animatePlane.js
@@ -3,7 +3,7 @@ var paper, map, spaceship, thruster, moon, flight_path, flight_path_length, last
         window.onload = function () {
             map                 = Snap('#svg-doc');
             spaceship           = document.getElementById("plane");
-            flight_path         = map.path('M 102.81244,304.05022 C 59.993073,364.58093 40.934293,277.19227 -16.236623,309.56167').attr({ 'fill': 'none', 'stroke': 'black'});
+            flight_path         = map.path('m 102.81244,304.05022 c -42.819367,60.53071 -51.008136,-7.41067 -41.967712,-2.19118 8.292577,4.78772 3.140906,27.02229 -25.750263,27.02229 -36.4164102,0 -47.179918,-20.03591 -48.890596,-22.99889').attr({ 'fill': 'none', 'stroke': 'none'});
             flight_path.style = "stroke: #ffffff";
             flight_path_length  = Snap.path.getTotalLength(flight_path);
             last_point          = flight_path.getPointAtLength(flight_path_length);
@@ -13,8 +13,8 @@ var paper, map, spaceship, thruster, moon, flight_path, flight_path_length, last
                     moveToPoint = Snap.path.getPointAtLength( flight_path, step );
                     x = moveToPoint.x;
                     y = moveToPoint.y;
-                    spaceship.style = "transform: translate(" + x + "vw, " + ((y - 300) / 1.5) + "vh) rotate(" + (moveToPoint.alpha) + "deg)";
-                },15000, mina.easeout ,function(){
+                    spaceship.style = "transform: translate(" + (x - 5) + "vw, " + ((y - 300) / 1.5) + "vh) rotate(" + (moveToPoint.alpha) + "deg)";
+                },15000, mina.linear ,function(){
                     animate();
                 });
             } 
