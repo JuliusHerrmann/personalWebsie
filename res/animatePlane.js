aa0c3493d1838bd3924d865e57e2e2d451d60fb2
Julius Herrmann <herrmann.julius@gmx.de>
2019-09-07 20:29:01 +0200

    path angepasst

--- res/animatePlane.js
+++ res/animatePlane.js
@@ -14,7 +14,7 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
             }
             map                 = Snap('#svg-doc');
             spaceship           = document.getElementById("plane");
-            flight_path         = map.path('m 102.81244,304.05022 c -42.819367,60.53071 -51.008136,-7.41067 -41.967712,-2.19118 8.292577,4.78772 12.070594,27.02229 -25.750263,27.02229 -36.4164102,0 -47.179918,-20.03591 -48.890596,-22.99889').attr({ 'fill': 'none', 'stroke': 'none'});
+            flight_path         = map.path('m 102.81244,304.05022 c -42.819367,60.53071 -51.008136,-7.41067 -41.967712,-2.19118 8.292577,4.78772 12.070594,27.02229 -25.750263,27.02229 -36.4164102,0 -50.1867,-20.7709 -51.897378,-23.73388').attr({ 'fill': 'none', 'stroke': 'none'});
             flight_path.style = "stroke: #ffffff";
             flight_path_length  = Snap.path.getTotalLength(flight_path);
             last_point          = flight_path.getPointAtLength(flight_path_length);
